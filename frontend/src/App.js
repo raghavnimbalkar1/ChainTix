09ba98b8114204bcc8c5037203360f998e830621
raghavnimbalkar1 <raghav.nimbalkar@gmail.com>
2024-11-11 19:51:16 +0530

    frontend-scroll

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,16 @@
-// src/App.js
+
 import React from 'react';
 import Navbar from './Navbar';
+import HeroSection from './HeroSection';
+import FeaturedEvents from './components/FeaturedEvents';
 import './App.css';
 
 function App() {
   return (
-    
     <div className="app">
       <Navbar />
-      <header className="hero-section">
-        <h2>Explore ChainTix</h2>
-        <div className="search-bar">
-          <input type="text" placeholder="Search for events, artists, or teams" />
-          <button>Submit</button>
-        </div>
-      </header>
-
-      <section className="featured-events">
-        <h3>Featured Events</h3>
-        <div className="event-card">
-          <h4>The Weekend</h4>
-          <p>Fri, Dec 1 at 8:00 PM</p>
-          <p>$100 - $500</p>
-        </div>
-
-      </section>
-      <section className="popular-collections">
-        <h3>Popular Collections</h3>
-        <div className='event-card'>
-          <h4>The Weekend</h4>
-          <p>Fri, Dec 3 at 8:30 PM</p>
-          <p>$80 - $100</p>
-        </div>
-
-      </section>
+      <HeroSection />
+      <FeaturedEvents />
     </div>
   );
 }
